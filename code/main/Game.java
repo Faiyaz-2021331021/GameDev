0d19dc72feaf862b60a1fb13d6b52fcc74bbd0ea
Faiyaz <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-08-12 17:03:49 +0600

    Updated Game.java

--- code/main/Game.java
+++ code/main/Game.java
@@ -1,13 +1,49 @@
 package main;
 
-public class Game
+public class Game implements Runnable
 {
+    private int frames=0;private long lastcheck=0;
     private GameWindow gamewindow;
     private GamePanel gamepanel;
+    private Thread gamethread;
+    private final int FPS = 120;
     public Game()
     {
         gamepanel = new GamePanel();
         gamewindow = new GameWindow(gamepanel);
         gamepanel.requestFocus();
-    }    
+        startgameloop();
+    }
+    
+    private void startgameloop()
+    {
+        gamethread = new Thread(this);
+        gamethread.start();
+    }
+
+    @Override
+    public void run()
+    {
+        double  timeperframe = 1000000000.0 / FPS;
+        long lastframe = System.nanoTime();
+        long now = System.nanoTime();
+        while(true)
+        {
+            now = System.nanoTime();
+            if(now - lastframe >= timeperframe)
+            {
+                gamepanel.repaint();
+                lastframe = now;
+                frames++;
+            }
+            if(System.currentTimeMillis() - lastcheck >= 1000)
+            {
+                lastcheck = System.currentTimeMillis();
+                System.out.println("FPS = " + frames);
+                frames=0;
+            }   
+        }
+    }
+
+    
 }
