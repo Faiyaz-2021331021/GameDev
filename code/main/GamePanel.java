8f67055b8eb70265a595ca4ee3c2371244656a8e
Faiyaz <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-08-13 18:12:54 +0600

    Update GamePanel.java
    Added character and movement animation to the character

--- code/main/GamePanel.java
+++ code/main/GamePanel.java
@@ -1,27 +1,72 @@
 package main;
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import Inputs.Keyboardinputs;
 import Inputs.Mouseinputs;
 import java.awt.Graphics;
-import java.util.Random;
-import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
+import java.awt.Dimension;
 
 public class GamePanel extends JPanel
 {
     private Mouseinputs mouseinputs;
     private float xdelta=100;private float ydelta=100;
-    
-    private float xdir = 1f;private float ydir=1f;
-    private Color color = new Color(50,80,200);
-    private Random random;
+    private BufferedImage img;
+    private BufferedImage[] idleani;
+    private int anitick,aniindex,anispeed = 30;
     
     public GamePanel()
     {
         mouseinputs = new Mouseinputs(this);
         addKeyListener(new Keyboardinputs(this));
+        setpanelsize();
         addMouseListener(mouseinputs);
         addMouseMotionListener(mouseinputs);
-        random = new Random();
+        importimage();
+        loadanimation();
+    }
+    private void loadanimation()
+    {
+        idleani = new BufferedImage[2];
+        // for(int i=0;i<5;i++)
+        // {
+        //     idleani[i] = img.getSubimage(10, 0, 35, 45);
+        // }
+        idleani[0] = img.getSubimage(10, 0, 35, 45);
+        idleani[1] = img.getSubimage(47, 0, 55, 45);
+    }
+    private void importimage()
+    {
+        InputStream is = getClass().getClassLoader().getResourceAsStream("res/Player.png");
+        try
+        {
+            img = ImageIO.read(is);
+        }
+        catch (IOException e)
+        {
+            // e.printStackTrace();
+            System.out.println("not found");
+        }
+        finally
+        {
+            try
+            {
+                is.close();
+            }
+            catch(IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void setpanelsize()
+    {
+        Dimension size = new Dimension(1280,800);
+        setMinimumSize(size);
+        setPreferredSize(size);
+        setMaximumSize(size);
     }
     public void changexdelta(int value)
     {
@@ -36,33 +81,26 @@ public class GamePanel extends JPanel
         this.xdelta = x;
         this.ydelta = y;
     }
-    private Color getrndcolor()
+    private void updateanimationtick()
     {
-        int r=random.nextInt(255);
-        int g = random.nextInt(255);
-        int b = random.nextInt(255);
-        return new Color(r,g,b);
-    }
-    private void updaterectangle()
-    {
-        xdelta+=xdir;
-        if(xdelta > 400 || xdelta < 0)
+        anitick++;
+        if(anitick >= anispeed)
         {
-            xdir*= - 1;
-            color = getrndcolor();
-        }
-        ydelta+=ydir;
-        if(ydelta > 400 || ydelta < 0)
-        {
-            ydir*= - 1;
-            color = getrndcolor();
+            anitick = 0;
+            aniindex++;
+            if(aniindex >= idleani.length)
+            {
+                aniindex = 0;
+            }
         }
     }
+    
     public void paintComponent(Graphics g)
     {
         super.paintComponent(g);
-        updaterectangle();
-        g.setColor(color);
-        g.fillRect((int)xdelta,(int)ydelta,200,50);
+        updateanimationtick();
+        g.drawImage(idleani[aniindex], (int)xdelta, (int)ydelta, 120,80,null);
+
+        
     }
 }
