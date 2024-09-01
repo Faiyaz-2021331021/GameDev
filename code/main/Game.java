161fa79fc81532d04b309c71f3ea9186de24bae7
Faiyaz Ismail <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-09-01 20:29:55 +0600

    Updated FPS and UPS

--- code/main/Game.java
+++ code/main/Game.java
@@ -1,5 +1,9 @@
 package main;
 
+import java.awt.Graphics;
+
+import Entities.Player;
+
 public class Game implements Runnable
 {
     private int frames=0;private long lastcheck=0;
@@ -7,41 +11,93 @@ public class Game implements Runnable
     private GamePanel gamepanel;
     private Thread gamethread;
     private final int FPS = 120;
+    private final int UPS_SET  = 200;
+    private Player player;
     public Game()
     {
-        gamepanel = new GamePanel();
+        initclasses();
+        gamepanel = new GamePanel(this);
         gamewindow = new GameWindow(gamepanel);
         gamepanel.requestFocus();
         startgameloop();
     }
     
+    private void initclasses()
+    {
+        player = new Player(200, 200);
+    }
+
     private void startgameloop()
     {
         gamethread = new Thread(this);
         gamethread.start();
     }
+    
+    public void render(Graphics g)
+    {
+        player.render(g);
+    }
+    
+    public void update()
+    {
+        player.update();
+    }
+
+
 
     @Override
     public void run()
     {
         double  timeperframe = 1000000000.0 / FPS;
-        long lastframe = System.nanoTime();
-        long now = System.nanoTime();
+        double timeperupdate = 1000000000.0 / UPS_SET;
+        long prevtime = System.nanoTime();
+
+        int frames = 0;
+        int updates = 0;
+        double deltau = 0;
+        double deltaf = 0;
+
+
         while(true)
         {
-            now = System.nanoTime();
-            if(now - lastframe >= timeperframe)
+            long currenttime = System.nanoTime();
+
+            deltau += (currenttime - prevtime) / timeperupdate;
+            deltaf += (currenttime - prevtime) / timeperframe;
+            prevtime = currenttime;
+
+            if(deltau >= 1)
+            {
+                update();
+                updates++;
+                deltau--;
+            }
+
+            if(deltaf >= 1)
             {
                 gamepanel.repaint();
-                lastframe = now;
+                deltaf--;
                 frames++;
             }
+
+
             if(System.currentTimeMillis() - lastcheck >= 1000)
             {
                 lastcheck = System.currentTimeMillis();
-                System.out.println("FPS = " + frames);
+                System.out.println("FPS = " + frames + " UPS = " + updates);
                 frames=0;
+                updates=0;
             }   
         }
+    }
+
+    public void windowfocuslost()
+    {
+        player.resetdirboolean();
+    }
+
+    public Player getPlayer()
+    {
+        return player;
     }
 }
