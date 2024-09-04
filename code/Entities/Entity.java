e04f4eeca2fd10ce6b5895d1295c5936c88f36da
Faiyaz Ismail <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-09-05 01:30:14 +0600

    Update Entity.java

---
+++ code/Entities/Entity.java
@@ -0,0 +1,14 @@
+package Entities;
+
+public abstract class Entity
+{
+    protected float x,y;
+    public Entity(float x,float y, int scalex, int scaley)
+    {
+        this.x = x;
+        this.y = y;
+    }
+    public Entity(float x2, float y2) {
+        //TODO Auto-generated constructor stub
+    }
+}
