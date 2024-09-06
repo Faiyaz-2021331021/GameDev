0dbb6d308f3427f61fbf36594b20d15dab85f0cb
Faiyaz Ismail <137979191+Faiyaz-2021331021@users.noreply.github.com>
2024-09-06 23:06:32 +0600

    Added border around player for boundary.java

--- code/Entities/Entity.java
+++ code/Entities/Entity.java
@@ -1,14 +1,47 @@
 package Entities;
 
+import java.awt.Rectangle;
+import java.awt.Graphics;
+import java.awt.Color;
+
 public abstract class Entity
 {
     protected float x,y;
-    public Entity(float x,float y, int scalex, int scaley)
+    protected int width,height;
+    protected Rectangle hitbox;
+
+    public Entity(float x,float y, int width, int height)
     {
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
+        inithitbox();
     }
-    public Entity(float x2, float y2) {
-        //TODO Auto-generated constructor stub
+
+    protected void drawhitbox(Graphics g)
+    {
+        g.setColor(Color.RED);
+        g.drawRect(hitbox.x+20, hitbox.y+3, hitbox.width+20, hitbox.height+10);
+
     }
+
+    private void inithitbox()
+    {
+       hitbox = new Rectangle((int)x,(int)y,width,height);
+    }
+
+
+    protected void updatehitbox()
+    {
+        hitbox.x = (int)x;
+        hitbox.y = (int)y;
+    }
+
+
+    public Rectangle  gethitbox()
+    {
+        return hitbox;
+    }
+
 }
